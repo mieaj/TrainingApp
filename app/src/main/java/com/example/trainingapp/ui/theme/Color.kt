26c4acea15ce78078078072a2d1d64e088cb4f97
Mie Agervig <agervig@live.dk>
2023-03-18 19:38:41 +0100

    Add theme

--- app/src/main/java/com/example/trainingapp/ui/theme/Color.kt
+++ app/src/main/java/com/example/trainingapp/ui/theme/Color.kt
@@ -2,10 +2,22 @@ package com.example.trainingapp.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple80 = Color(0xFFD0BCFF)
-val PurpleGrey80 = Color(0xFFCCC2DC)
-val Pink80 = Color(0xFFEFB8C8)
+val LightBackground = Color(0xFFF2F5F7)
+val LightSurface = Color(0xFFFEFFFF)
+val LightSurfaceVariant = Color(0xFFE4E5E6)
+val LightOnBackground = Color(0xCC000000)
+val LightOnSurface = Color(0xA6000000)
+val LightOnSurfaceVariant = Color(0x80000000)
 
-val Purple40 = Color(0xFF6650a4)
-val PurpleGrey40 = Color(0xFF625b71)
-val Pink40 = Color(0xFF7D5260)
+val DarkBackground = Color(0xFF1F2024)
+val DarkSurface = Color(0xFF2A2E30)
+val DarkSurfaceVariant = Color(0xFF474B4D)
+val DarkOnBackground = Color(0xF3FFFFFF)
+val DarkOnSurface = Color(0xC0FFFFFF)
+val DarkOnSurfaceVariant = Color(0x8CFFFFFF)
+
+val Primary = Color(0xFF1D9160)
+
+val Disabled = Color(0x62FFFFFF)
+val Error = Color(0xFFE74C3C)
+val Success = Color(0xFF2ECC71)
