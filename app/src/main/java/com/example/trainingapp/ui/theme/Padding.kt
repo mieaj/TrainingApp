1e42586403b3b42fc38ef6dd1f21b0efa2f047da
Mie Agervig <agervig@live.dk>
2023-05-19 00:13:47 +0200

    Prepare for UI

---
+++ app/src/main/java/com/example/trainingapp/ui/theme/Padding.kt
@@ -0,0 +1,9 @@
+package com.example.trainingapp.ui.theme
+
+import androidx.compose.ui.unit.dp
+
+val ExtraSmallPadding = 4.dp
+val SmallPadding = 8.dp
+val MediumPadding = 16.dp
+val LargePadding = 24.dp
+val ExtraLargePadding = 32.dp
---
+++ app/src/main/java/com/example/trainingapp/ui/theme/components/Buttons.kt
@@ -0,0 +1,30 @@
+package com.example.trainingapp.ui.theme.components
+
+import androidx.compose.runtime.Composable
+
+@Composable
+fun IconButton() {
+
+}
+
+@Composable
+fun RaisedButton() {
+
+}
+/*
+
+Example
+@Composable
+fun Greeting(name: String) {
+    Text(text = "Hello $name!")
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    TrainingAppTheme {
+        Greeting("Android")
+    }
+}
+
+*/
