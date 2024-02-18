96846c33d78e940a3fc62554c2074250c287c1dc
Mie Agervig <agervig@live.dk>
2024-02-18 13:35:20 +0100

    Clean up graph function

--- app/src/main/java/com/example/trainingapp/ui/BodyTrackerScreen.kt
+++ app/src/main/java/com/example/trainingapp/ui/BodyTrackerScreen.kt
@@ -1,37 +1,23 @@
 package com.example.trainingapp.ui
 
-import android.graphics.Paint
-import android.graphics.PointF
-import androidx.compose.foundation.Canvas
-import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.staggeredgrid.LazyVerticalStaggeredGrid
 import androidx.compose.foundation.lazy.staggeredgrid.StaggeredGridCells
-import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.geometry.Offset
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.drawscope.DrawScope
-import androidx.compose.ui.graphics.nativeCanvas
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
 import com.example.trainingapp.R
-import com.example.trainingapp.ui.theme.LargePadding
 import com.example.trainingapp.ui.theme.MediumPadding
-import com.example.trainingapp.ui.theme.Shapes
 import com.example.trainingapp.ui.theme.SmallPadding
+import com.example.trainingapp.ui.theme.components.Graph
 import com.example.trainingapp.ui.theme.components.OutlinedIconCard
 import com.ramcosta.composedestinations.annotation.Destination
 
@@ -77,103 +63,4 @@ fun BodyTrackerScreen() {
     }
 }
 
-@Composable
-fun Graph(
-    modifier: Modifier = Modifier,
-    xValues: List<Int> = listOf(1, 2, 3, 4, 5),
-    yValues: List<Int> = listOf(2, 3, 4, 5, 6, 7, 8),
-    points: List<Float> = listOf(2f, 3f, 2f, 4f, 8f)
-) {
-    val pointColor = MaterialTheme.colorScheme.primary
-    val background = MaterialTheme.colorScheme.surface
-    val coordinateSystemColor = MaterialTheme.colorScheme.onBackground
-    val numberLineColor = MaterialTheme.colorScheme.onSurface
-    val lineColor = MaterialTheme.colorScheme.onSurfaceVariant
 
-    Box(
-        modifier = modifier
-            .background(background, shape = Shapes.medium)
-            .padding(horizontal = LargePadding, vertical = LargePadding),
-        contentAlignment = Alignment.Center
-    ) {
-        Canvas(
-            modifier = Modifier
-                .fillMaxSize(),
-        ) {
-            val xAxisSpace = size.width / xValues.size
-            val yAxisSpace = size.height / yValues.size
-            val startPoint = PointF(0f, size.height - 30)
-            drawCoordinateSystem(coordinateSystemColor, startPoint)
-            /** placing x axis points */
-            xValues.forEachIndexed { i, value ->
-                drawContext.canvas.nativeCanvas.drawText(
-                    "$value",
-                    xAxisSpace * (i + 1),
-                    startPoint.y,
-                    Paint().apply {
-                        textSize = 12.sp.toPx()
-                    }
-                )
-            }
-            /** placing y axis points */
-            yValues.forEachIndexed { i, value ->
-                drawContext.canvas.nativeCanvas.drawText(
-                    "$value",
-                    startPoint.x,
-                    size.height - yAxisSpace * (i + 1),
-                    Paint().apply {
-                        textSize = 12.sp.toPx()
-                    }
-                )
-                drawLine(
-                    color = numberLineColor,
-                    start = Offset(0f + 60f, size.height - yAxisSpace * (i + 1)),
-                    end = Offset(size.width + 40f, size.height - yAxisSpace * (i + 1)),
-                    strokeWidth = 2f
-                )
-            }
-
-            /** placing points */
-            var currentPoint: Pair<Float, Float>? = null
-            points.forEachIndexed { i, value ->
-                val x1 = xAxisSpace * (i + 1)
-                val y1 = size.height - (yAxisSpace * (value - (yValues.min() - 1)))
-                drawCircle(
-                    color = pointColor,
-                    radius = 10f,
-                    center = Offset(x1, y1)
-                )
-                currentPoint?.let { (x, y) ->
-                    drawLine(
-                        color = lineColor,
-                        start = Offset(x, y),
-                        end = Offset(x1, y1),
-                        strokeWidth = 4f
-                    )
-                }
-                currentPoint = x1 to y1
-
-            }
-
-        }
-    }
-
-}
-
-fun DrawScope.drawCoordinateSystem(color: Color, startPointF: PointF) {
-    val lineSize = 6f
-    val xAxeOffset = 70f
-    val yAxeOffset = 60f
-    drawLine(
-        color = color,
-        start = Offset(startPointF.x + yAxeOffset, size.height - xAxeOffset),
-        end = Offset(0f + yAxeOffset, 0f - 40f),
-        strokeWidth = lineSize
-    )
-    drawLine(
-        color = color,
-        start = Offset(0f + yAxeOffset, size.height - xAxeOffset),
-        end = Offset(size.width + 40f, size.height - xAxeOffset),
-        strokeWidth = lineSize
-    )
-}
---
+++ app/src/main/java/com/example/trainingapp/ui/theme/components/Graph.kt
@@ -0,0 +1,156 @@
+package com.example.trainingapp.ui.theme.components
+
+import android.graphics.Paint
+import android.graphics.PointF
+import androidx.compose.foundation.Canvas
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.drawscope.DrawScope
+import androidx.compose.ui.graphics.nativeCanvas
+import androidx.compose.ui.unit.sp
+import com.example.trainingapp.ui.theme.LargePadding
+import com.example.trainingapp.ui.theme.Shapes
+
+@Composable
+fun Graph(
+    modifier: Modifier = Modifier,
+    points: List<Float> = listOf(10f, 30f, 25f, 15f, 20f)
+) {
+    val rangeArea = if (points.max() - points.min() > 10) 5 else 1
+    val xValues = ((1..10))
+    val yValues = (points.min().toInt()-rangeArea..points.max().toInt()+rangeArea step rangeArea)
+    val pointColor = MaterialTheme.colorScheme.primary
+    val background = MaterialTheme.colorScheme.surface
+    val coordinateSystemColor = MaterialTheme.colorScheme.onBackground
+    val numberLineColor = MaterialTheme.colorScheme.onSurface
+    val lineColor = MaterialTheme.colorScheme.onSurfaceVariant
+
+    Box(
+        modifier = modifier
+            .background(background, shape = Shapes.medium)
+            .padding(horizontal = LargePadding, vertical = LargePadding),
+        contentAlignment = Alignment.Center
+    ) {
+        Canvas(
+            modifier = Modifier
+                .fillMaxSize(),
+        ) {
+            val axisSpace = PointF(size.width / xValues.count(), size.height / yValues.count())
+            val startPoint = PointF(0f, size.height)
+            val axisOffset = 40f
+            val numberOffset = 1f
+
+            drawCoordinateSystem(
+                color = coordinateSystemColor,
+                axisOffset = axisOffset,
+                startPointF = startPoint
+            )
+            drawCoordinateNumber(
+                color = numberLineColor,
+                numberOffset = numberOffset,
+                xValues = xValues,
+                yValues = yValues,
+                axisOffset = axisOffset,
+                startPoint = startPoint,
+                axisSpace = axisSpace
+            )
+            /** placing points */
+            /** placing points */
+            /** placing points */
+
+            /** placing points */
+            var currentPoint: Pair<Float, Float>? = null
+            points.forEachIndexed { i, value ->
+                val x1 = axisSpace.x * (i + numberOffset)
+                val y1 =
+                    startPoint.y - ((axisSpace.y / rangeArea) * (value - yValues.min() + rangeArea))
+                drawCircle(
+                    color = pointColor,
+                    radius = 12f,
+                    center = Offset(x1, y1)
+                )
+                currentPoint?.let { (x, y) ->
+                    drawLine(
+                        color = lineColor,
+                        start = Offset(x, y),
+                        end = Offset(x1, y1),
+                        strokeWidth = 4f
+                    )
+                }
+                currentPoint = x1 to y1
+            }
+
+        }
+    }
+
+}
+
+fun DrawScope.drawCoordinateSystem(color: Color, axisOffset: Float, startPointF: PointF) {
+    val lineSize = 6f
+    drawLine(
+        color = color,
+        start = Offset(startPointF.x + axisOffset, startPointF.y - axisOffset),
+        end = Offset(startPointF.x + axisOffset, -axisOffset),
+        strokeWidth = lineSize
+    )
+    drawLine(
+        color = color,
+        start = Offset(startPointF.x + axisOffset, startPointF.y - axisOffset),
+        end = Offset(size.width + axisOffset, startPointF.y - axisOffset),
+        strokeWidth = lineSize
+    )
+}
+
+fun DrawScope.drawCoordinateNumber(
+    color: Color,
+    numberOffset: Float,
+    xValues: IntRange,
+    yValues: IntProgression,
+    axisOffset: Float,
+    startPoint: PointF,
+    axisSpace: PointF
+) {
+    val textPaint = Paint().apply {
+        textSize = 12.sp.toPx()
+    }
+    /** placing x axis points */
+    xValues.forEachIndexed { i, value ->
+        drawContext.canvas.nativeCanvas.drawText(
+            "$value",
+            axisSpace.x * (i + numberOffset),
+            startPoint.y,
+            textPaint
+        )
+    }
+    /** placing y axis points */
+    yValues.forEachIndexed { i, value ->
+        if (i % 2 != 0) {
+            drawContext.canvas.nativeCanvas.drawText(
+                "$value",
+                startPoint.x,
+                startPoint.y - axisSpace.y * (i + numberOffset),
+                textPaint
+            )
+        }
+        drawLine(
+            color = color,
+            start = Offset(
+                startPoint.x + axisOffset,
+                startPoint.y - axisSpace.y * (i + numberOffset)
+            ),
+            end = Offset(
+                size.width + axisOffset,
+                startPoint.y - axisSpace.y * (i + numberOffset)
+            ),
+            strokeWidth = 2f
+        )
+    }
+}
