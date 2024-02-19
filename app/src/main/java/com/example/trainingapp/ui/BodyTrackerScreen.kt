1f4ff9569c2048e541ceb0b1dd66886cfcccaa4b
Mie Agervig <agervig@live.dk>
2024-02-19 20:40:07 +0100

    Add Weight

--- app/src/main/java/com/example/trainingapp/ui/BodyTrackerScreen.kt
+++ app/src/main/java/com/example/trainingapp/ui/BodyTrackerScreen.kt
@@ -2,21 +2,33 @@ package com.example.trainingapp.ui
 
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.staggeredgrid.LazyVerticalStaggeredGrid
 import androidx.compose.foundation.lazy.staggeredgrid.StaggeredGridCells
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material3.Button
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.text.input.TextFieldValue
 import androidx.compose.ui.unit.dp
 import com.example.trainingapp.R
 import com.example.trainingapp.ui.theme.LargePadding
 import com.example.trainingapp.ui.theme.MediumPadding
+import com.example.trainingapp.ui.theme.Shapes
 import com.example.trainingapp.ui.theme.SmallPadding
 import com.example.trainingapp.ui.theme.components.Graph
 import com.example.trainingapp.ui.theme.components.OutlinedIconCard
@@ -48,7 +60,7 @@ fun BodyTrackerScreen() {
         modifier = Modifier
             .fillMaxSize()
             .padding(MediumPadding),
-        verticalArrangement = Arrangement.spacedBy(LargePadding),
+        verticalArrangement = Arrangement.SpaceBetween,
     ) {
 
         LazyVerticalStaggeredGrid(
@@ -66,11 +78,45 @@ fun BodyTrackerScreen() {
                             value = it.value,
                             selected = it.topic == selectedItem
                         ) {
+                            if (HEIGHT == it.topic) return@OutlinedIconCard
                             selectedItem = it.topic
                         }
                     }
                 }
+                    item {
+                        Spacer(modifier = Modifier.height(MediumPadding))
+                        if (selectedItem== WEIGHT) {
+                            Row(
+                                modifier = Modifier.fillMaxWidth(),
+                                verticalAlignment = Alignment.CenterVertically,
+                                horizontalArrangement = Arrangement.spacedBy(SmallPadding),
+                            ) {
+                                var text by remember { mutableStateOf(TextFieldValue("")) }
+                                OutlinedTextField(
+                                    modifier = Modifier.weight(1f),
+                                    value = text,
+                                    maxLines = 1,
+                                    shape = Shapes.medium,
+                                    onValueChange = {
+                                        text = it
+                                    },
+                                    label = { Text(text = "Update ${selectedItem}") },
+                                    placeholder = { Text(text = "E.g: 10") },
+                                    keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number)
+                                )
+                                Button(
+                                    modifier = Modifier, onClick = { },
+                                    shape = Shapes.medium
+                                ) {
+                                    Text(maxLines = 1, text = "Update")
+                                }
+                            }
+                        }
+                    }
+
+//        InfiniteCircularList(items = (1..10).toMutableList(), initialItem = 4)
             })
+        if (selectedItem == HEIGHT||selectedItem.isEmpty()) return
         Graph(
             modifier = Modifier
                 .fillMaxWidth()
